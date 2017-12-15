f9cfc750d7462d2f9216f4ca52dc740b60845f21
UniMulRock <net.neron.9839ner@gmail.com>
2017-12-15 22:08:52 +0900

    settingwindow実装

---
+++ Assets/Scripts/System/Window/CompletionWindow.cs
@@ -0,0 +1,18 @@
+﻿
+using Utility.System;
+using UnityEngine;
+
+namespace Utility.Window
+{
+    public class CompletionWindow : WindowBase
+    {
+        public void OnClose()
+        {
+            if (WindowManager.Validation())
+            {
+                WindowManager.Instance.CloseWindow(WindowManager.WINDOW_TYPE.COMPLETION);
+            }
+        }
+    }
+}
+
--- Assets/Scripts/System/Window/WindowBase.cs
+++ Assets/Scripts/System/Window/WindowBase.cs
@@ -1,43 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using System;
 
 namespace Utility.Window
 {
     public class WindowBase : MonoBehaviour
     {
         [SerializeField]
-        private GameObject ScreenRoot;
+        private CanvasGroup ScreenRoot;
 
         private const float SCALE_DURATION = 0.5f;
+        private const float FADE_IN_VALUE = 1f;
+        private const float FADE_OUT_VALUE = 0f;
+        private const float FADE_DURATION = 0.5f;
 
         private void Awake()
         {
             if (ScreenRoot != null)
             {
-                if(ScreenRoot.activeSelf)
-                    ScreenRoot.SetActive(false);
+                if (ScreenRoot.gameObject.activeSelf)
+                    ScreenRoot.gameObject.SetActive(false);
                 ScreenRoot.transform.localScale = Vector3.zero;
             }
         }
 
         [ContextMenu("Open")]
-        virtual public void Open()
+        virtual public void Open(Action callback = null)
         {
             if (ScreenRoot == null)
                 return;
 
-            ScreenRoot.SetActive(true);
+            ScreenRoot.gameObject.SetActive(true);
 
             Sequence seq = DOTween.Sequence();
             seq.Append
             (
                 ScreenRoot.transform.DOScale(Vector3.one, SCALE_DURATION)
-            ).OnComplete(()=>{});
+            ).Join(
+                ScreenRoot.DOFade(FADE_IN_VALUE, FADE_DURATION)
+            ).OnComplete(() =>
+                {
+                    if (callback != null)
+                    {
+                        callback();
+                    }
+                });
         }
 
         [ContextMenu("Close")]
-        virtual public void Close()
+        virtual public void Close(Action callback = null)
         {
             if (ScreenRoot == null)
                 return;
@@ -45,10 +57,16 @@ namespace Utility.Window
             seq.Append
             (
                 ScreenRoot.transform.DOScale(Vector3.zero, SCALE_DURATION)
+            ).Join(
+                ScreenRoot.DOFade(FADE_OUT_VALUE, FADE_DURATION)
             ).OnComplete(() =>
-            {
-                ScreenRoot.SetActive(false);
-            });
+                {
+                    ScreenRoot.gameObject.SetActive(false);
+                    if (callback != null)
+                    {
+                        callback();
+                    }
+                });
         }
     }
 }
