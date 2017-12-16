759bd228cbd802445f563e5ebe6e800077d0d15c
UniMulRock <net.neron.9839ner@gmail.com>
2017-12-16 12:38:14 +0900

    ステージキャラ作成

---
+++ Assets/Scripts/Stage/StageGenerater.cs
@@ -0,0 +1,28 @@
+﻿using Utility.System;
+using UnityEngine;
+
+namespace PeeMax.Stage
+{
+    public class StageGenerater : SingletonMonoBehaviour<StageGenerater>
+    {
+        private const string STAGE_DIC = "Stage/";
+
+        public GameObject CreateStage(Transform root, string stageName)
+        {
+            var characterResource = Resources.Load(STAGE_DIC + stageName) as GameObject;
+            if (characterResource == null)
+            {
+                Debug.LogError("【ロード失敗】" + STAGE_DIC + stageName);
+                return null;
+            }
+            var instanceCharacter = Instantiate(characterResource, root) as GameObject;
+            if (instanceCharacter == null)
+            {
+                Debug.LogError("【生成失敗】" + STAGE_DIC + stageName);
+                return null;
+            }
+            instanceCharacter.name = stageName;
+            return instanceCharacter;
+        }
+    }
+}
---
+++ Assets/Scripts/System/Scene/GameSceneSystem.cs
@@ -0,0 +1,40 @@
+﻿using Utility.System;
+using UnityEngine;
+using PeeMax.Char;
+using PeeMax.Stage;
+
+namespace System.Scene
+{
+    public class GameSceneSystem : SingletonMonoBehaviour<GameSceneSystem>
+    {
+        /// <summary>
+        /// WindowRoot
+        /// </summary>
+        [SerializeField]
+        private GameObject windowRoot;
+
+        [SerializeField]
+        private GameObject stageRoot;
+
+        [SerializeField]
+        private GameObject charRoot;
+
+        protected override void Awake()
+        {
+            if (WindowManager.Validation())
+            {
+                WindowManager.Instance.WindowRoot = windowRoot;
+            }
+
+            if (CharGenerater.Validation())
+            {
+                CharGenerater.Instance.CreateChar(charRoot.transform);
+            }
+
+            if (StageGenerater.Validation())
+            {
+                StageGenerater.Instance.CreateStage(stageRoot.transform,"test1");
+            }
+        }
+    }
+}
