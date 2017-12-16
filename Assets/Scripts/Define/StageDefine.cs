44d7b63ace1ac80a7a7a30c08cdaad84bb880c47
UniMulRock <net.neron.9839ner@gmail.com>
2017-12-16 16:15:04 +0900

    iconをCSVから読み込む

--- Assets/Scripts/Define/StageDefine.cs
+++ Assets/Scripts/Define/StageDefine.cs
@@ -6,6 +6,8 @@
 
         public const string STAGE_DATA_CSV = "StageData";
 
+        public const string ICON_DATA_CSV = "IconData";
+
         public const int STAGE_SELECT_BUTTOM_MAX = 15;
 
         public const string STAGE_SELECT_DIC = "StageSelect/";
@@ -13,5 +15,11 @@
         public const string STAGE_LIST_ROOT = "StageListRoot";
 
         public const string STAGE_SELECT_BUTTON = "StageSelectButton";
+
+        public const string IMAGE_DIC = "Images/";
+
+        public const string GAME_DIC = "Game/";
+
+        public const string ICON_DIC = "Icon/";
     }
 }
